21e4018a9a5ffa9ea10206a914acbeda6ae401f1
sivhuo <sprak@macalester.edu>
2019-03-31 18:34:59 -0500

    Adding Background to FrontPage

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import styles from './FrontPage.css';
 
 const FrontPage = ({ onStartButton }) => {
   return (
-    <div className="tc">
-      <h1>Welcome to Pizar!</h1>
-      <h2>Build your favorite pizza and we'll find you a movie!</h2>
+    <div className={styles.FrontPage}>
+      <h1 class="f-headline lh-solid">Welcome to Pizar!</h1>
+      <h2
+      className="tc f2 fw4">Tell us your favourite pizzar and we'll tell you what movie to watch today!</h2>
+      <h3
+      className="tc f5 fw4">They might not seem related but they really are.</h3>
       <button
-      className="f3 fw6 link ph3 pv2 mb2 dib white bg-dark-blue"
-      onClick={onStartButton}>Start!</button>
+      className="tc f3 fw4 link ph3 pv2 mb2 dib black bg-dark-red"
+      onClick={onStartButton}>BUILD YOUR PIZAR</button>
     </div>
   )
 }
