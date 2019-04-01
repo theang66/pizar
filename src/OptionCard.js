58c8cf044bfda8c89964f85f07d5622b4c4bbba2
sivhuo <sprak@macalester.edu>
2019-04-01 00:20:28 -0500

    Changing Background Image

--- src/OptionCard.js
+++ src/OptionCard.js
@@ -6,7 +6,7 @@ const OptionCard = ({ onClick, image, name, attributes, text }) => {
     <div onClick={onClick}
     attributes={attributes} text={text}
     className="tc f2 bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="options" src={require(`../pics/${image}`)} height="200px" width="200px"/>
+      <img alt="options" src={require(`../pics/${image}`)} height="250px" width="250px"/>
       <p>{name}</p>
     </div>
   );
