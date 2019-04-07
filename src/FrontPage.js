66169c7b6b40b0972b8be9b1bbce9dfab7f655e4
theang66 <anguyen1@macalester.edu>
2019-04-07 15:55:41 -0500

    Changed the start button and added to App.css

--- src/FrontPage.js
+++ src/FrontPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import styles from './FrontPage.css';
 
 const FrontPage = ({ onStartButton }) => {
   return (
-    <div className={styles.FrontPage}>
+    <div>
       <h1 className="f-headline lh-solid">Welcome to Pizar!</h1>
       <h2
       className="tc f2 fw4">Tell us your favorite pizza and we'll tell you what movie to watch!</h2>
       <h2
       className="tc f3 fw4">They might not seem related but they really are.</h2>
-      <button
-      className="tc f3 fw4 link ph3 pv2 mb2 grow dib black bg-dark-red"
-      onClick={onStartButton}>BUILD YOUR PIZZA</button>
+      <img className="tc grow"
+      alt="options" src={require(`./background/roundpizza.png`)}
+      width="300px"
+      onClick={onStartButton}/>
     </div>
   )
 }
--- src/OptionCard.js
+++ src/OptionCard.js
@@ -5,7 +5,7 @@ const OptionCard = ({ onClick, image, name, attributes, text }) => {
   return (
     <div onClick={onClick}
     attributes={attributes} text={text}
-    className="tc f2 bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
+    className="tc f3 bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="options" src={require(`../pics/${image}`)} height="250px" width="250px"/>
       <p>{name}</p>
     </div>
