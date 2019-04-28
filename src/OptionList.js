08a74ceda7fd4bcb30643348c053f511af67723e
sivhuo <sprak@macalester.edu>
2019-04-28 15:49:48 -0500

    Fixing the gap between optionCards

--- src/OptionList.js
+++ src/OptionList.js
@@ -8,20 +8,40 @@ class OptionList extends Component {
   render() {
     const shuffledOptions = this.props.type.slice(0, 4);
     return(
+      <div>
       <Grid container spacing={0} style={{padding: 0}}
       alignItems="center"
       justify="center">
-        { shuffledOptions.map(option => (
-          <Grid key={option.id} item xs={6} sm={6} lg={6} xl={6}>
             <OptionCard
-            key={option.id}
-            name={option.name}
-            image={option.image}
-            onClick={this.props.handleClick(option.attributes, option.text) }
+            key={shuffledOptions[0].id}
+            name={shuffledOptions[0].name}
+            image={shuffledOptions[0].image}
+            onClick={this.props.handleClick(shuffledOptions[0].attributes, shuffledOptions[0].text) }
             />
-          </Grid>
-        ))}
+            <OptionCard
+            key={shuffledOptions[1].id}
+            name={shuffledOptions[1].name}
+            image={shuffledOptions[1].image}
+            onClick={this.props.handleClick(shuffledOptions[1].attributes, shuffledOptions[1].text) }
+            />
       </Grid>
+      <Grid container spacing={0} style={{padding: 0}}
+            alignItems="center"
+            justify="center">
+            <OptionCard
+            key={shuffledOptions[2].id}
+            name={shuffledOptions[2].name}
+            image={shuffledOptions[2].image}
+            onClick={this.props.handleClick(shuffledOptions[2].attributes, shuffledOptions[2].text) }
+            />
+            <OptionCard
+            key={shuffledOptions[3].id}
+            name={shuffledOptions[3].name}
+            image={shuffledOptions[3].image}
+            onClick={this.props.handleClick(shuffledOptions[3].attributes, shuffledOptions[3].text) }
+            />
+      </Grid>
+      </div>
     )
   }
 }
