36fd3bc1983594c89f5925f2d93577106daaa0ad
theang66 <anguyen1@macalester.edu>
2019-05-01 02:01:37 -0500

    Server for deploying

--- src/OptionCard.js
+++ src/OptionCard.js
@@ -6,7 +6,7 @@ const OptionCard = ({ onClick, image, name, attributes, text, style }) => {
   return (
     <div className="container" style={style} onClick={onClick}
       attributes={attributes} text={text}
-      className="tc f4 bg-animate hover-bg-dark-red bg-near-white dib br1 pa1 ma1 grow bw2 shadow-5">
+      className="tc f4 bg-animate hover-bg-dark-red bg-near-white dib br1 pa2 ma1 grow bw2 shadow-5">
       <img alt="options" src={require(`./pics/${image}`)} height="250px" width="350px"/>
       <div className="text-block">
         <p>{name}</p>
