dfa9de342d064e852c5138c9bbc5080916bd83b3
Dan Nguyen <dnguyen2@macalester.edu>
2019-05-03 00:42:48 -0500

    Fixed resizing problems on the quiz screen

--- src/OptionList.js
+++ src/OptionList.js
@@ -11,34 +11,34 @@ class OptionList extends Component {
         <div className="row justify-content-md-center">
           <div className="col-sm-8">
            	<div className="row">
-           		<div className="col-sm-6">
+           		<div className="col-lg-6 col-md-12">
                 <OptionCard
-                 key={shuffledOptions[0].id}
-                 name={shuffledOptions[0].name}
-                 image={shuffledOptions[0].image}
-                 onClick={this.props.handleClick(shuffledOptions[0].attributes, shuffledOptions[0].text, shuffledOptions[0].id)}
-                 style={shuffledOptions[0].id === this.props.chosen ? { background: 'red' } : null}
-                 />
+                  key={shuffledOptions[0].id}
+                  name={shuffledOptions[0].name}
+                  image={shuffledOptions[0].image}
+                  onClick={this.props.handleClick(shuffledOptions[0].attributes, shuffledOptions[0].text, shuffledOptions[0].id)}
+                  style={shuffledOptions[0].id === this.props.chosen ? { background: 'red' } : null}
+                />
            		</div>
-           		<div className="col-sm-6">
-                 <OptionCard
-                 key={shuffledOptions[1].id}
-                 name={shuffledOptions[1].name}
-                 image={shuffledOptions[1].image}
-                 onClick={this.props.handleClick(shuffledOptions[1].attributes, shuffledOptions[1].text, shuffledOptions[1].id)}
-                 style={shuffledOptions[1].id === this.props.chosen ? { background: 'red' } : null}
-                 />
+           		<div className="col-lg-6 col-md-12">
+                <OptionCard
+                  key={shuffledOptions[1].id}
+                  name={shuffledOptions[1].name}
+                  image={shuffledOptions[1].image}
+                  onClick={this.props.handleClick(shuffledOptions[1].attributes, shuffledOptions[1].text, shuffledOptions[1].id)}
+                  style={shuffledOptions[1].id === this.props.chosen ? { background: 'red' } : null}
+                />
            		</div>
-           		<div className="col-sm-6">
-                 <OptionCard
-                 key={shuffledOptions[2].id}
-                 name={shuffledOptions[2].name}
-                 image={shuffledOptions[2].image}
-                 onClick={this.props.handleClick(shuffledOptions[2].attributes, shuffledOptions[2].text, shuffledOptions[2].id)}
-                 style={shuffledOptions[2].id === this.props.chosen ? { background: 'red' } : null}
-                 />
+           		<div className="col-lg-6 col-md-12">
+                <OptionCard
+                  key={shuffledOptions[2].id}
+                  name={shuffledOptions[2].name}
+                  image={shuffledOptions[2].image}
+                  onClick={this.props.handleClick(shuffledOptions[2].attributes, shuffledOptions[2].text, shuffledOptions[2].id)}
+                  style={shuffledOptions[2].id === this.props.chosen ? { background: 'red' } : null}
+                />
            		</div>
-           		<div className="col-sm-6">
+           		<div className="col-lg-6 col-md-12">
                  <OptionCard
                  key={shuffledOptions[3].id}
                  name={shuffledOptions[3].name}
